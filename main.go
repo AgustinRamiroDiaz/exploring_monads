8bb2c0cfd902b0beccbf60fd35f206ef4b806661
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2022-08-11 08:02:41 -0300

    extend to generic twice

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 
-	x := newValue(1)
-	y := x.Then(func(a int) Result[int] {
-		return newValue(a + 1)
-	}).Then(func(a int) Result[int] {
-		return newValue(a + 1)
-	}).Then(func(a int) Result[int] {
-		return newError[int](fmt.Errorf("nope"))
-	}).Then(func(a int) Result[int] {
-		return newValue(a + 1)
+	x := newValue[int, int](1)
+	y := x.Then(func(a int) Result[int, int] {
+		return newValue[int, int](a + 1)
+	}).Then(func(a int) Result[int, int] {
+		return newValue[int, int](a + 1)
+	}).Then(func(a int) Result[int, int] {
+		return newError[int, int](fmt.Errorf("nope"))
+	}).Then(func(a int) Result[int, int] {
+		return newValue[int, int](a + 1)
 	})
 
 	value, err := y.Value(), y.Error()
@@ -21,30 +21,30 @@ func main() {
 	fmt.Printf("%+v - %+v\n", value, err)
 }
 
-type Result[A any] struct {
+type Result[A, B any] struct {
 	value A
 	err   error
 }
 
-func newError[A any](err error) Result[A] {
-	return Result[A]{err: err}
+func newError[A, B any](err error) Result[A, B] {
+	return Result[A, B]{err: err}
 }
 
-func newValue[A any](value A) Result[A] {
-	return Result[A]{value: value}
+func newValue[A, B any](value A) Result[A, B] {
+	return Result[A, B]{value: value}
 }
 
-func (r Result[A]) Value() A {
+func (r Result[A, B]) Value() A {
 	return r.value
 }
 
-func (r Result[A]) Error() error {
+func (r Result[A, B]) Error() error {
 	return r.err
 }
 
-func (r Result[A]) Then(f func(A) Result[A]) Result[A] {
+func (r Result[A, B]) Then(f func(A) Result[A, B]) Result[A, B] {
 	if r.err != nil {
-		return newError[A](r.err)
+		return newError[A, B](r.err)
 	}
 	return f(r.value)
 }
